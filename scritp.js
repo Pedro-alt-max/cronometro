f1f472fb6565faff4fa78e9ea73dd87387045848
Pedro Felipe <pedrofelipeazevedo@hotmail.com>
2023-02-24 17:58:20 -0300

    Commit inicial

---
+++ scritp.js
@@ -0,0 +1,84 @@
+const minutesEl = document.querySelector("#minutes");
+const secondsEl = document.querySelector("#seconds");
+const millisecondsEl = document.querySelector("#milliseconds");
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resumeBtn = document.querySelector("#resumeBtn");
+const resertBtn = document.querySelector("#resetBtn");
+
+let interval;
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let isPaused = false;
+
+startBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click", pauseTime);
+resumeBtn.addEventListener("click", resumeTime);
+resertBtn.addEventListener("click", resertTime);
+
+
+function startTimer() {
+  console.log("Start");
+  isPaused = false;
+  interval = setInterval(() => {
+    if (!isPaused) {
+      milliseconds += 10;
+
+      if (milliseconds === 1000) {
+        seconds++;
+        milliseconds = 0;
+      }
+
+      if (seconds === 60) {
+        minutes++;
+        seconds = 0;
+      }
+
+      minutesEl.textContent = formatTime(minutes);
+      secondsEl.textContent = formatTime(seconds);
+      millisecondsEl.textContent = formMilliseconds(milliseconds);
+    }
+  }, 10);
+
+  startBtn.style.display = 'none';
+  pauseBtn.style.display = "block";
+
+  /* startBtn.getElementsByClassName.display = "none";
+  pauseBtn.getElementsByClassName.display = "block"; */
+}
+
+function pauseTime() {
+  isPaused = true;
+  pauseBtn.style.display = "none"
+  resumeBtn.style.display = "block";
+}
+
+function resumeTime() {
+  isPaused = false;
+  pauseBtn.style.display = "block";
+  resumeBtn.style.display = "none";
+}
+function resertTime() {
+  clearInterval(interval);
+  minutes = 0;
+  seconds = 0;
+  milliseconds = 0;
+
+  minutesEl.textContent = formatTime(minutes);
+  secondsEl.textContent = formatTime(seconds);
+  millisecondsEl.textContent = formMilliseconds(milliseconds);
+
+  startBtn.style.display = "block";
+  pauseBtn.style.display = "none";
+  resumeBtn.style.display = "none";
+
+}
+
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+function formMilliseconds(time) {
+  return time < 100 ? `${time}`.padStart(3,"0") : time;
+}
